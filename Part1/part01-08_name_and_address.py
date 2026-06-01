fbad537712dc0154979d4ef73e498a69a7539ecc
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-01 20:58:32 -0300

    Create part01-08_name_and_address.py

---
+++ Part1/part01-08_name_and_address.py
@@ -0,0 +1,8 @@
+	# Write your solution here
+	f_name = input("Given name?: ")
+	l_name = input("Family name?: ")
+	street = input("Street address: ")
+	z_code = input("City and postal code: ")
+	print(f_name + " " + l_name)
+	print(street)
+	print(z_code)

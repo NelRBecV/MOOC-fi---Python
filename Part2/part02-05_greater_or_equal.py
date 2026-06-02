0d6545f02a8215509a82cd6a260758102cca2298
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-02 10:39:12 -0300

    Create part02-05_greater_or_equal.py

---
+++ Part2/part02-05_greater_or_equal.py
@@ -0,0 +1,9 @@
+first_num = int(input("Please type in the first number:"))
+	secnd_num = int(input("Please type in the second number:"))
+	if first_num > secnd_num:
+	    print(f"The greater number was {first_num}")
+	elif first_num < secnd_num:
+	    print(f"The greater number was {secnd_num}")
+	else:
+	    print("The numbers are equal!")
+

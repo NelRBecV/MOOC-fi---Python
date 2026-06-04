482be9f2d075257342178b883b02648bd9826885
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-03 22:07:40 -0300

    Create part03-27_taking_turns.py

---
+++ Part3/part03-27_taking_turns.py
@@ -0,0 +1,15 @@
+number = int(input("Please type in a number:"))
+	count = 0
+	rev = 0
+	while True:
+	    rev=number-count
+	    count+=1    
+	    if rev >= count:
+	        if rev == count:
+	            print(count)
+	        else:
+	            print(count)
+	            print(rev)
+	    else:        
+	        break
+        

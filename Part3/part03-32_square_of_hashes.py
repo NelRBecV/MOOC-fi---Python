40e04b293f8c6e9949e973ac6f0f3358aa6d01f5
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-03 22:12:46 -0300

    Create part03-32_square_of_hashes.py

---
+++ Part3/part03-32_square_of_hashes.py
@@ -0,0 +1,10 @@
+# Write your solution here
+	def hash_square(num:int):
+	    i=0    
+	    while i < num:        
+	        print("#"*num)
+	        i+=1
+	# You can test your function by calling it within the following block
+	if __name__ == "__main__":
+	    hash_square(5)
+    

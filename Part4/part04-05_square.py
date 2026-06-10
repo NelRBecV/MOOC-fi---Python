43f8e2f21f43473a62d634595bcf7a97b4d77e25
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-10 09:54:22 -0300

    Create part04-05_square.py

---
+++ Part4/part04-05_square.py
@@ -0,0 +1,13 @@
+# Copy here code of line function from previous exercise
+	def line(times:int,char:str):
+	    if char == "":
+	        char="*"
+	    print(char[0]*times)
+	def square(size, character):
+	    # You should call function line here with proper parameters
+	    for s in range(size):
+	        line(size, character)
+	 
+	# You can test your function by calling it within the following block
+	if __name__ == "__main__":
+	    square(8, "p")

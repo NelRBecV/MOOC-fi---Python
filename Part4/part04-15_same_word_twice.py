dcc972cd3d79f84db9034763c8de33ec51dbb82d
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-10 10:27:20 -0300

    Create part04-15_same_word_twice.py

---
+++ Part4/part04-15_same_word_twice.py
@@ -0,0 +1,9 @@
+words:list = []
+	while True:
+	    word = input("Word: ")
+	    if word not in words:
+	        words.append(word)
+	    else:
+	        print(f"You typed in {len(words)} different words")
+	        break
+        
